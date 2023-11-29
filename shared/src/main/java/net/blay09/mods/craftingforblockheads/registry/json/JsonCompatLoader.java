e00afccdcb90b3ef61ba69adbc25fd2dc17f038f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-11-29 20:55:01 +0100

    feat: Load providers from CraftingForBlockheadsRegistry.json in addition to datapacks, allow multiple providers in a single json

--- shared/src/main/java/net/blay09/mods/craftingforblockheads/registry/json/JsonCompatLoader.java
+++ shared/src/main/java/net/blay09/mods/craftingforblockheads/registry/json/JsonCompatLoader.java
@@ -2,6 +2,7 @@ package net.blay09.mods.craftingforblockheads.registry.json;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.craftingforblockheads.CraftingForBlockheadsConfig;
@@ -10,15 +11,14 @@ import net.blay09.mods.craftingforblockheads.api.CraftingForBlockheadsProvider;
 import net.blay09.mods.craftingforblockheads.registry.DataDrivenProviderFactory;
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.FileToIdConverter;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraft.world.item.crafting.Ingredient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.*;
 
 public class JsonCompatLoader implements ResourceManagerReloadListener {
@@ -31,20 +31,47 @@ public class JsonCompatLoader implements ResourceManagerReloadListener {
 
     @Override
     public void onResourceManagerReload(ResourceManager resourceManager) {
-        for (CraftingForBlockheadsProvider providersFromDataPack : providersFromDataPacks) {
+        for (final var providersFromDataPack : providersFromDataPacks) {
             CraftingForBlockheadsAPI.unregisterProvider(providersFromDataPack);
         }
         providersFromDataPacks.clear();
 
-        for (Map.Entry<ResourceLocation, Resource> entry : COMPAT_JSONS.listMatchingResources(resourceManager).entrySet()) {
-            try (BufferedReader reader = entry.getValue().openAsReader()) {
-                CraftingForBlockheadsProvider gridProvider = load(JsonProviderData.fromJson(gson.fromJson(reader, JsonObject.class)));
-                if (gridProvider != null) {
-                    providersFromDataPacks.add(gridProvider);
-                }
-            } catch (Exception e) {
-                logger.error("Parsing error loading CraftingTweaks data file at {}", entry.getKey(), e);
+        final var simpleFile = new File(Balm.getConfig().getConfigDir(), "CraftingForBlockheadsRegistry.json");
+        if (simpleFile.exists()) {
+            try (final var reader = new FileReader(simpleFile)) {
+                final var gridProviders = load(gson.fromJson(reader, JsonElement.class));
+                providersFromDataPacks.addAll(gridProviders);
+            } catch (IOException e) {
+                logger.error("Parsing error loading Crafting for Blockheads data file at {}", simpleFile, e);
             }
+        } else {
+            try {
+                Files.writeString(simpleFile.toPath(), "[]");
+            } catch (IOException ignored) {
+            }
+        }
+
+        for (final var entry : COMPAT_JSONS.listMatchingResources(resourceManager).entrySet()) {
+            try (final var reader = entry.getValue().openAsReader()) {
+                final var gridProviders = load(gson.fromJson(reader, JsonElement.class));
+                providersFromDataPacks.addAll(gridProviders);
+            } catch (Exception e) {
+                logger.error("Parsing error loading Crafting for Blockheads data file at {}", entry.getKey(), e);
+            }
+        }
+    }
+
+    private static List<CraftingForBlockheadsProvider> load(JsonElement jsonElement) {
+        if (jsonElement.isJsonObject()) {
+            return Collections.singletonList(load(JsonProviderData.fromJson(jsonElement.getAsJsonObject())));
+        } else if (jsonElement.isJsonArray()) {
+            final var providers = new ArrayList<CraftingForBlockheadsProvider>();
+            for (JsonElement element : jsonElement.getAsJsonArray()) {
+                providers.addAll(load(element));
+            }
+            return providers;
+        } else {
+            throw new IllegalArgumentException("Invalid Crafting for Blockheads data, expected json object or array");
         }
     }
 
