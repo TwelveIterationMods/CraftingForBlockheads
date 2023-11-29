07cce1092de6654ead8bb6271bb29bc2a4b33bf1
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-11-29 20:54:12 +0100

    feat: Sorting based on weapon/armor tier

--- shared/src/main/java/net/blay09/mods/craftingforblockheads/util/CraftableComparator.java
+++ shared/src/main/java/net/blay09/mods/craftingforblockheads/util/CraftableComparator.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.craftingforblockheads.util;
 
 import net.blay09.mods.craftingforblockheads.crafting.RecipeWithStatus;
+import net.minecraft.world.item.*;
 
 import java.util.Comparator;
 
@@ -8,32 +9,33 @@ public class CraftableComparator implements Comparator<RecipeWithStatus> {
 
     @Override
     public int compare(RecipeWithStatus o1, RecipeWithStatus o2) {
-        // Checking for missing ingredients and predicates for both objects
-        boolean o1HasMissingIngredients = !o1.missingIngredients().isEmpty();
-        boolean o1HasMissingPredicates = !o1.missingPredicates().isEmpty();
-        boolean o2HasMissingIngredients = !o2.missingIngredients().isEmpty();
-        boolean o2HasMissingPredicates = !o2.missingPredicates().isEmpty();
-
-        // Compare based on missing predicates and ingredients
-        if (!o1HasMissingIngredients && !o1HasMissingPredicates && (o2HasMissingIngredients || o2HasMissingPredicates)) {
-            // o1 has neither missing ingredients nor predicates but o2 does
-            return -1;
-        } else if (!o2HasMissingIngredients && !o2HasMissingPredicates && (o1HasMissingIngredients || o1HasMissingPredicates)) {
-            // o2 has neither missing ingredients nor predicates but o1 does
-            return 1;
-        } else if (!o1HasMissingPredicates && o2HasMissingPredicates) {
-            // o1 only has missing ingredients, but o2 has missing predicates or both
-            return -1;
-        } else if (!o2HasMissingPredicates && o1HasMissingPredicates) {
-            // o2 only has missing ingredients, but o1 has missing predicates or both
-            return 1;
-        }
-
-        // If both are in the same category, compare based on display names
-        String s1 = o1.resultItem().getDisplayName().getString();
-        String s2 = o2.resultItem().getDisplayName().getString();
-        return s1.compareToIgnoreCase(s2);
+        return Comparator
+                .comparing((RecipeWithStatus r) -> !r.missingIngredients().isEmpty() || !r.missingPredicates().isEmpty())
+                .thenComparing(r -> !r.missingPredicates().isEmpty())
+                .thenComparing(r -> getTierLevel(r.resultItem()))
+                .thenComparing(r -> r.resultItem().getDisplayName().getString(), String.CASE_INSENSITIVE_ORDER)
+                .compare(o1, o2);
     }
 
+    private int getTierLevel(ItemStack itemStack) {
+        if (itemStack.getItem() instanceof TieredItem tieredItem) {
+            final var tier = tieredItem.getTier();
+            return (tier instanceof Tiers tiers) ? tiers.ordinal() : tier.getLevel();
+        } else if (itemStack.getItem() instanceof ArmorItem armorItem) {
+            return armorItem.getMaterial() instanceof ArmorMaterials armorMaterials ? getArmorTier(armorMaterials) : 0;
+        }
+        return 0;
+    }
+
+    private int getArmorTier(ArmorMaterials armorMaterials) {
+        return switch (armorMaterials) {
+            case LEATHER -> Tiers.STONE.ordinal();
+            case GOLD -> Tiers.GOLD.ordinal();
+            case CHAIN, IRON -> Tiers.IRON.ordinal();
+            case DIAMOND -> Tiers.DIAMOND.ordinal();
+            case NETHERITE -> Tiers.NETHERITE.ordinal();
+            default -> 0;
+        };
+    }
 
 }
