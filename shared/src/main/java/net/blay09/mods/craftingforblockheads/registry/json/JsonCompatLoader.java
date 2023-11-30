db59440cad9edbd9ea7aba82c205916d9a8ed094
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-12-01 00:37:55 +0100

    fix: Fix error on empty data file, closes #15

--- shared/src/main/java/net/blay09/mods/craftingforblockheads/registry/json/JsonCompatLoader.java
+++ shared/src/main/java/net/blay09/mods/craftingforblockheads/registry/json/JsonCompatLoader.java
@@ -14,6 +14,7 @@ import net.minecraft.resources.FileToIdConverter;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -61,7 +62,11 @@ public class JsonCompatLoader implements ResourceManagerReloadListener {
         }
     }
 
-    private static List<CraftingForBlockheadsProvider> load(JsonElement jsonElement) {
+    private static List<CraftingForBlockheadsProvider> load(@Nullable JsonElement jsonElement) {
+        if (jsonElement == null) {
+            return Collections.emptyList();
+        }
+
         if (jsonElement.isJsonObject()) {
             if (jsonElement.getAsJsonObject().keySet().isEmpty()) {
                 return Collections.emptyList();
